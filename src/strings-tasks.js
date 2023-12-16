bfff3b7e5d7faef18a4f19a30be6df914e67abee
Andrei Huryn <gurin.nord@gmail.com>
2023-12-16 15:37:52 +0300

    feat: solve some tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -229,8 +229,8 @@ function sumOfCodes(str) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -244,8 +244,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
@@ -261,8 +261,16 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  let min = String(minutes);
+  let sec = String(seconds);
+  if (min.length === 1) {
+    min = '0'.concat(min);
+  }
+  if (sec.length === 1) {
+    sec = '0'.concat(sec);
+  }
+  return `${min}:${sec}`;
 }
 
 /**
@@ -275,8 +283,12 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    result += str.charAt(i);
+  }
+  return result;
 }
 
 /**
